b6e1c7cc92c817cb22999218b9a729755c82acb4
Tom-Phillip Thomsen <tom.thomsen@neogeo.com>
2022-11-18 19:58:36 +0100

    Added ambience sounds to the background

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ interlude_bars = 2
 
 
 live_loop :flibble do
+  cue :flibble_go
   part_1_bars. times do
     sample :guit_em9, rate: 0.5
     sleep 4
@@ -16,6 +17,7 @@ live_loop :flibble do
     sample :guit_e_fifths
     sleep 4
   end
+  cue :flibble_pause
   interlude_bars. times do
     sleep 4
   end
@@ -61,4 +63,24 @@ define :beatVariation do
   sleep 1
   sample :drum_bass_soft
   sleep 0.5
+end
+
+ambience_notes = scale(:E3, :minor)
+ambience_amp = 0.75
+
+in_thread(name: :ambience) do
+  use_synth :dark_ambience
+  ambience_note = play ambience_notes.pick, sustain: 2000.000 , note_slide: 1, amp: ambience_amp, amp_slide: 1
+  
+  in_thread(name: :flibble_watcher) do
+    sync :flibble_pause
+    control ambience_note, amp: ambience_amp * 0.25
+    sync :flibble_go
+    control ambience_note, amp: ambience_amp
+  end
+  
+  loop do
+    sleep 4
+    control ambience_note, note: ambience_notes.pick
+  end
 end
